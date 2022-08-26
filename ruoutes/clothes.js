881400f283b4fb2b9db573d991549492feb24843
Gian Diaz <49536327+GianDiazArce@users.noreply.github.com>
2022-08-26 14:47:15 -0500

    Ruta Update agregada

--- ruoutes/clothes.js
+++ ruoutes/clothes.js
@@ -84,9 +84,59 @@ const saveInCloudinary = async (file) => {
   const { secure_url } = await cloudinary.uploader.upload(file.filepath);
   return secure_url;
 };
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const form = new formidable.IncomingForm();
+  let title = "";
+  let description = "";
+  let imageURL = "";
+  let hasImage;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(400).json({ ok: false, msg: "No es un id valido" });
+
+  const clothes = await ClothesModel.findById(id);
+  if (!clothes) {
+    return res.status(404).json({ msg: `No existe esa ropa con el id: ${id}` });
+  }
+
+  form.parse(req, async (err, fields, files) => {
+    if (!fields.title) {
+      return res.status(400).send({ msg: "El titulo es obligatorio" });
+    }
+    if (!files.file) hasImage = false;
+    else hasImage = true;
+    if (fields.file === clothes.image) imageURL = clothes.image;
+    title = fields.title;
+    description = fields.description;
+    imageURL = hasImage
+      ? await saveInCloudinary(files.file)
+      : fields.file === clothes.image
+      ? clothes.image
+      : fields.file;
+    try {
+      const toUpdate = {
+        title: title || clothes.title,
+        description: description || clothes.description,
+        image: imageURL,
+      };
+      const updateClothes = await ClothesModel.findByIdAndUpdate(id, toUpdate, {
+        new: true,
+      });
+      return res.status(200).json({
+        ok: true,
+        updatePost: updateClothes,
+      });
+    } catch (error) {
+      return res.status(500).json({ msg: "Error check server logs", error });
+    }
+  });
+});
 
 router.delete("/:id", async (req, res) => {
   const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(400).json({ ok: false, msg: "No es un id valido" });
   const clothes = await ClothesModel.findById(id);
   if (!clothes) {
     return res.status(404).json({ msg: `No existe esa ropa con el id: ${id}` });
